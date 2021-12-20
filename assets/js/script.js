00d6c47b5fba26730b36bf6aac2337937aad90a9
areum <kar3509@gmail.com>
2021-12-20 17:03:46 +0900

    2021.12.20

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,15 +23,21 @@ $(document).ready(function(){
   });
 
   // mixitup
-  $(function(){
+  // $(function(){
     var mixer = mixitup('.portfolio-graphic');
-  });
+  // });
 
+  
   
   // hamburger
   $('.hamburger').click(function(){
-    $(this).toggleClass('active')
+    $(this).toggleClass('active');
   });
+
+  $('.dep1-arrow').click(function(){
+    $('.dep2').toggleClass('active');
+  });
+  
 
 
   // lightbox
@@ -42,33 +48,85 @@ $(document).ready(function(){
 
 
   // magnific popup
-  $('.mfp-motion').magnificPopup({
-    type: 'image',
-    closeOnContentClick: true,
-    closeBtnInside: true,
-    fixedConentPos: true,
-    image: { verticalFit: true },
-    gallery: { enabled: true },
-    zoom: { enabled: true, duration: 400 },
+  $('.graphic .btn').on('click', function(){
+    console.log($(this).data('filter'));
+    switch ($(this).data('filter')) {
+      case '.motion': 
+        popMotion();
+        break;
+      case '.illust':
+        popIllust();
+        break;
+      case '.emoticon': 
+        popEmoticon();
+        break;
+      case 'all':
+        popAll();
+        break;
+    }
   });
 
-  $('.mfp-illust').magnificPopup({
-    type: 'image',
-    closeOnContentClick: true,
-    closeBtnInside: true,
-    fixedConentPos: true,
-    image: { verticalFit: true },
-    gallery: { enabled: true },
-    zoom: { enabled: true, duration: 400 },
-  });
 
-  $('.mfp-emoticon').magnificPopup({
-    type: 'image',
-    closeOnContentClick: true,
-    closeBtnInside: true,
-    fixedConentPos: true,
-    image: { verticalFit: true },
-    gallery: { enabled: true },
-    zoom: { enabled: true, duration: 400 },
-  });
+
+  function popAll() {
+    console.log('ALL');
+    // 3
+    $('.mfp').magnificPopup({
+      type: 'image',
+      closeOnContentClick: true,
+      closeBtnInside: true,
+      fixedConentPos: true,
+      image: { verticalFit: true },
+      gallery: { enabled: true },
+      zoom: { enabled: true, duration: 400 },
+    });
+  }
+
+  function popMotion() {
+    $('.mfp-motion').magnificPopup({
+      type: 'image',
+      closeOnContentClick: true,
+      closeBtnInside: true,
+      fixedConentPos: true,
+      image: { verticalFit: true },
+      gallery: { enabled: true },
+      zoom: { enabled: true, duration: 400 },
+    });
+  }
+
+  function popIllust() {
+    $('.mfp-illust').magnificPopup({
+      type: 'image',
+      closeOnContentClick: true,
+      closeBtnInside: true,
+      fixedConentPos: true,
+      image: { verticalFit: true },
+      gallery: { enabled: true },
+      zoom: { enabled: true, duration: 400 },
+    });
+  }
+
+  function popEmoticon() {
+    $('.mfp-emoticon').magnificPopup({
+      type: 'image',
+      closeOnContentClick: true,
+      closeBtnInside: true,
+      fixedConentPos: true,
+      image: { verticalFit: true },
+      gallery: { enabled: true },
+      zoom: { enabled: true, duration: 400 },
+    });
+  }
+
+
+  // $('.mfp').magnificPopup({
+  //   type: 'image',
+  //   closeOnContentClick: true,
+  //   closeBtnInside: true,
+  //   fixedConentPos: true,
+  //   image: { verticalFit: true },
+  //   gallery: { enabled: true },
+  //   zoom: { enabled: true, duration: 400 },
+  // });
+
 })
