3459d6a51d85d109d371c640e6e22360a6d13f10
areum <kar3509@gmail.com>
2021-12-24 17:40:41 +0900

    2021.12.24

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,4 +36,46 @@ $(document).ready(function(){
   $('.dep1-arrow').click(function(){
     $('.dep2').toggleClass('active');
   }); 
+
+
+  // 퀵버튼 숨기기
+  
+
+  // $(window).resize(function(){
+    function detectMobileDevice() {
+      const minWidth = 720
+    
+      return window.innerWidth <= minWidth
+    }
+    
+    const isMobile = detectMobileDevice()
+
+    if (isMobile) {
+      var isVisible = false;
+  
+      $('.quick').addClass('hide');
+      $(window).on('scroll',function(){
+        if (checkVisible($('.kv'))&&!isVisible) {
+          console.log('kv영역');
+          $('.quick').addClass('hide');
+          isVisible=true;
+        }
+    
+        else 
+          $('.quick').removeClass('hide');
+          isVisible=false
+      });
+    
+      function checkVisible( elm, eval ) {
+        eval = eval || "object visible";
+        var viewportHeight = $(window).height(), // Viewport Height
+            scrolltop = $(window).scrollTop(), // Scroll Top
+            y = $(elm).offset().top,
+            elementHeight = $(elm).height();   
+        
+        if (eval == "object visible") return ((y < (viewportHeight + scrolltop)) && (y > (scrolltop - elementHeight)));
+        if (eval == "above") return ((y < (viewportHeight + scrolltop)));
+      }
+    }
+  // });
 })
